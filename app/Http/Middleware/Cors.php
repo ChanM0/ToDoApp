709a1de3463b397f5fe15eb7e7cbeccd045b04b9
chanM0 <mikec201722@gmail.com>
2018-11-13 20:05:30 -0800

    Able to accept request from a different server

---
+++ app/Http/Middleware/Cors.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class Cors
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        return $next($request)
+            // Access-Control-Allow-Origin header is missing when running into a cors error on front end
+            // * any outer domain may access my routes . 
+            ->header('Access-Control-Allow-Origin', '*')
+            // Access-Control-Allow-Methods allows certain methods 
+            // Options is an extra request that the browser generates and is sent
+            ->header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS')
+            // Access-Control-Allow-Headers allows certain headers  
+            ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    }
+}
